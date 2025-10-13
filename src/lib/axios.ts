dc3543875491cb244aefceca820ffd7bcbb79daf
Василий <a1@MacBook-Pro-1.local>
2025-10-13 21:44:50 +0300

    Смена api-адреса

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://92.51.39.215:8080',
   headers: {
     'Content-Type': 'application/json',
   },
